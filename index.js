618a17458f93cc610d16533b826f71c7af13857c
Simon <simonanewton@gmail.com>
2020-05-01 15:29:20 -0400

    1.4 updated questions and generateMarkdown functionality

--- index.js
+++ index.js
@@ -6,29 +6,57 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 const questions = [
 	{
 		type: "input",
-		message: "Enter your GitHub username:",
-		name: "username",
-		default: "simonanewton",
-	},
-	{
-		type: "input",
 		message: "Enter the title of your project:",
 		name: "title",
-		default: "Example Project Title",
+		default: "Example Project Title"
 	},
 	{
 		type: "input",
 		message: "Enter a description of your project:",
 		name: "description",
-		default: "Example Project Description",
+		default: "Example Project Description"
+	},
+	{
+		type: "input",
+		message: "Enter a description of the steps required to install your project:",
+		name: "installation",
+		default: "Example Project Installation"
+	},
+	{
+		type: "input",
+		message: "Enter a description of how your project is intended to be used:",
+		name: "usage",
+		default: "Example Project Usage"
+	},
+	{
+		type: "list",
+		message: "Select a license to utilise for your project:",
+		choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"],
+		name: "license"
+	},
+	{
+		type: "input",
+		message: "Enter a description of how other developers can contribute to your project:",
+		name: "contributing",
+		default: "Example Project Contributing"
+	},
+	{
+		type: "input",
+		message: "Enter any tests that you have made for your project and describe how to utilise them:",
+		name: "tests",
+		default: "Example Project Tests"
+	},
+	{
+		type: "input",
+		message: "Enter your GitHub username:",
+		name: "username",
+		default: "simonanewton"
 	}
 ];
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (error) => {
-        if (error) console.log(error);
-
-        console.log("Success!");
+        console.log(error ? error : "Success!");
     })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,37 @@ function generateMarkdown(data) {
 	# ${data.title}
 
 	## Description
+
 	${data.description}
+
+	## Table of Contents
+
+	* [Installation](#installation)
+	* [Usage](#usage)
+	* [License](#license)
+	* [Contributing](#contributing)
+	* [Tests](#tests)
+	* [Questions](#questions)
+
+	## Installation
+
+	${data.installation}
+
+	## Usage
+
+	${data.usage}
+
+	## License
+
+	${data.license}
+
+	## Contributing
+
+	${data.contributing}
+
+	## Tests
+
+	${data.tests}
 
 	## User Information
 	GitHub Username: @${data.username}
