1f0cddf7d204807af32c6e0b083327acaa66936f
Simon <simonanewton@gmail.com>
2020-05-01 05:12:41 -0400

    1.1 added inital inquirer functionality

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const questions = [
+const inquirer = require("inquirer");
 
+const questions = [
+    {
+		type: "input",
+		message: "Enter your GitHub username:",
+		name: "username",
+		default: "simonanewton",
+    },
+    {
+        type: "input",
+        message: "Enter the title of your project:",
+        name: "title",
+        default: "Example Project Title"
+    }
 ];
 
 function writeToFile(fileName, data) {
 }
 
-function init() {
-
+async function init() {
+    try {
+        const userInput = await inquirer.prompt(questions);
+        console.log(userInput);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
 init();
