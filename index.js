b3001990985132ca63d863f9a12eb99b5684ab16
Simon <simonanewton@gmail.com>
2020-05-01 05:31:15 -0400

    1.2 added initial writeToFile functionality

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
 
 const questions = [
-    {
+	{
 		type: "input",
 		message: "Enter your GitHub username:",
 		name: "username",
 		default: "simonanewton",
-    },
-    {
-        type: "input",
-        message: "Enter the title of your project:",
-        name: "title",
-        default: "Example Project Title"
-    }
+		validate: (response) => response.length === 0 ? console.log("Please enter a GitHub username.") : true
+	},
+	{
+		type: "input",
+		message: "Enter the title of your project:",
+		name: "title",
+		default: "Example Project Title",
+		validate: (response) => response.length === 0 ? console.log("Please enter a project title.") : true
+	},
+	{
+		type: "input",
+		message: "Enter a description of your project:",
+		name: "description",
+		default: "Example Project Description",
+		validate: (response) => response.length === 0 ? console.log("Please enter a project description.") : true
+	}
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        if (error) console.log(error);
+
+        console.log("Success!");
+    })
 }
 
 async function init() {
     try {
         const userInput = await inquirer.prompt(questions);
-        console.log(userInput);
+        
+        writeToFile("example.md", userInput);
     }
     catch (error) {
         console.log(error);
